ef81fe7a647c79f08e38a9bee3a4eefbab23534b
xiaozhang14 <xiaozhang14@iflytek.com>
2023-06-29 09:59:00 +0800

    fix: 入口文件去业务化

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,4 @@
-import PluginBase from './lib/PluginBase';
-import WindowControlPlugin from './lib/windowControl';
-import FileHelperPlugin from './lib/FileHelper';
-import HttpHelperPlugin from './lib/HttpHelper';
-import ShellHelperPlugin from './lib/ShellHelper';
-import ClientStoragePlugin from './lib/ClientStorage';
-const { isCef, getAppConfig, getExePath, getHomeUrl, setZoomLevel } = PluginBase;
+import HelloWorld from './lib/HelloWorld';
 export default {
-  // 静态方法
-  isCef,
-  getAppConfig,
-  getExePath,
-  getHomeUrl,
-  setZoomLevel,
-  // 构造器对象
-  WindowControlPlugin,
-  FileHelperPlugin,
-  HttpHelperPlugin,
-  ShellHelperPlugin,
-  ClientStoragePlugin,
+  HelloWorld
 };
